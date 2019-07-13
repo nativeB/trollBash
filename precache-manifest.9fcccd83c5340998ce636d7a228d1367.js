7c8804d58c918851a2a09fa38541f82291aed0b4
QUINCY HUTCHISON <30450645+nativeB@users.noreply.github.com>
2019-07-13 16:02:28 +0000

    Updates

---
+++ precache-manifest.9fcccd83c5340998ce636d7a228d1367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442c464717e6c2cea2e337ff2fe354d4",
+    "url": "/nativeB/trollBash.git/index.html"
+  },
+  {
+    "revision": "dfa092b6a46646f374eb",
+    "url": "/nativeB/trollBash.git/static/css/main.a7ee08e4.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/nativeB/trollBash.git/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "dfa092b6a46646f374eb",
+    "url": "/nativeB/trollBash.git/static/js/main.cb4ffb1b.chunk.js"
+  },
+  {
+    "revision": "d9f4ac4c74cf51d53484",
+    "url": "/nativeB/trollBash.git/static/js/runtime~main.1af8149a.js"
+  },
+  {
+    "revision": "dc5366dd5ef9560b252e5196a2bb331e",
+    "url": "/nativeB/trollBash.git/static/media/troll.dc5366dd.jpg"
+  }
+]);
